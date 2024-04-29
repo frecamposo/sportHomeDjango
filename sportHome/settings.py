deaefaeef53fa3d9026386a8406cbf64eab6dd29
Freddy <you@example.com>
2024-04-29 15:39:40 -0400

    ok

--- sportHome/settings.py
+++ sportHome/settings.py
@@ -78,20 +78,21 @@ WSGI_APPLICATION = 'sportHome.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } 
+ "default": {
+        "ENGINE": "django.db.backends.oracle",
+        "NAME": "orcl",
+        "USER": "sportHome",
+        "PASSWORD": "sportHome",
+        "HOST": "localhost",
+        "PORT": "1522",
+    }
 }
 
-#  "default": {
-#         "ENGINE": "django.db.backends.oracle",
-#         "NAME": "orcl",
-#         "USER": "sportHome",
-#         "PASSWORD": "sportHome",
-#         "HOST": "localhost",
-#         "PORT": "1522",
-#     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # } 
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
