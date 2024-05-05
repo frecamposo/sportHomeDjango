b0b729671a7eb9f4b549cc0b4ccb620f319e3bed
frecamposo <42188039+frecamposo@users.noreply.github.com>
2024-05-04 20:35:33 -0400

    Update settings.py

--- sportHome/settings.py
+++ sportHome/settings.py
@@ -78,20 +78,20 @@ WSGI_APPLICATION = 'sportHome.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
- "default": {
-        "ENGINE": "django.db.backends.oracle",
-        "NAME": "orcl",
-        "USER": "sportHome2024",
-        "PASSWORD": "sportHome2024",
-        "HOST": "localhost",
-        "PORT": "1522",
-    }
-}
+    'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     } 
 
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # } 
+}
+ #"default": {
+ #       "ENGINE": "django.db.backends.oracle",
+ #       "NAME": "orcl",
+ #       "USER": "sportHome2024",
+ #       "PASSWORD": "sportHome2024",
+ #       "HOST": "localhost",
+ #       "PORT": "1522",
+ #   }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
